8731866924e6744903e79f7b28a8f0a37ff2bcb9
NestorPala <npalavecino@fi.uba.ar>
2023-02-25 23:28:55 -0300

    Initial commit

---
+++ src/main/java/com/pala/NotesAppSpringBoot/NotesAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.pala.NotesAppSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pala/NotesAppSpringBoot/NotesAppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pala.NotesAppSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
